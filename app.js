e816f181bc87592f36e9d0193d4dc6a23248864c
limbert <limbert_90_lab@hotmail.com>
2016-11-15 22:20:50 -0400

    6ta correccion para openshift

--- app.js
+++ app.js
@@ -62,7 +62,7 @@ app.use(function(err, req, res, next) {
 //   console.log( "Listening on " + ip + ", port " + port )
 // });
 
-app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8080);
+app.set('port', process.env.OPENSHIFT_NODEJS_PORT || 8000);
 app.set('ip', process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1');
 
 http.createServer(app).listen(app.get('port'), app.get('ip'), function(){

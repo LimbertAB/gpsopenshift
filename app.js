c009456cb3905aa4a90e413ef2d66b8ca77122b9
limbert <limbert_90_lab@hotmail.com>
2016-11-17 18:16:49 -0400

    10mo commit para openshift

--- app.js
+++ app.js
@@ -55,17 +55,10 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-// var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
-// var ip = process.env.OPENSHIFT_NODEJS_IP;
- 
-// app.listen(port, ip, function () {
-//   console.log( "Listening on " + ip + ", port " + port )
-// });
+var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
+var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
 
-// var server_port = process.env.OPENSHIFT_NODEJS_PORT || 8080
-// var server_ip_address = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'
- 
-// app.listen(server_port, server_ip_address, function () {
-//   console.log( "Listening on " + server_ip_address + ", port " + server_port )
-// });
+app.listen(server_port, server_ip_address, function(){
+  console.log("Listening on " + server_ip_address + ", server_port " + server_port)
+});
 module.exports = app;
